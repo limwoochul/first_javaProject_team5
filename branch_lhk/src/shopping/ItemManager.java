52a06e19af8ff6abebbba3c047eba4ed7a08e896
hxkyu <hxkyu@naver.com>
2024-07-26 18:01:44 +0900

    수정 업데이트

---
+++ branch_lhk/src/shopping/ItemManager.java
@@ -0,0 +1,36 @@
+package shopping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemManager {
+
+	private List<Item> itemList = new ArrayList<Item>();
+	private int nextItemNumber = 1;
+	
+	public List<Item> getItemList() {
+		return itemList;
+	}
+	
+	public void addItem(Item item) {
+		itemList.add(item);
+		nextItemNumber++;
+	}
+	
+	public void removeItem(Item item) {
+		itemList.remove(item);
+	}
+	
+	public Item getItemNumber(int itemNumber) {
+		for(Item item : itemList) {
+			if(item.getItemNumber() == itemNumber) {
+				return item;
+			}
+		}
+		return null;
+	}
+	
+	public int getNextItemNumber() {
+		return nextItemNumber;
+	}
+}
